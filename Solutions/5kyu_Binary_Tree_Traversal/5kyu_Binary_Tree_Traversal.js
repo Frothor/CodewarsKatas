1667e4a5cd2a2b28dced274fdda6784da402e445
Morphix <pmjakubiak13@gmail.com>
2020-07-22 23:09:41 +0200

    added another solution for 5kyu_Binary_Tree_Traversal.

--- Solutions/5kyu_Binary_Tree_Traversal/5kyu_Binary_Tree_Traversal.js
+++ Solutions/5kyu_Binary_Tree_Traversal/5kyu_Binary_Tree_Traversal.js
@@ -46,3 +46,32 @@ function postOrder(node)
     arr.push(node.data);
   }
 }
+
+
+//Another solution:
+// 1.) Root node, 2.) traverse left subtree, 3.) traverse right subtree.
+function preOrder(node)
+{
+  if (node == undefined) {
+    return [];
+  }
+  return [node.data].concat(preOrder(node.left)).concat(preOrder(node.right));
+}
+
+// 1.) Traverse left subtree, 2.) root node, 3.) traverse right subtree.
+function inOrder(node)
+{
+  if (node == undefined) {
+    return [];
+  }
+  return inOrder(node.left).concat(node.data).concat(inOrder(node.right));
+}
+
+// 1.) Traverse left subtree, 2.) traverse right subtree, 3.) root node.
+function postOrder(node)
+{
+  if (node == undefined) {
+    return [];
+  }
+  return postOrder(node.left).concat(postOrder(node.right)).concat([node.data]);
+}
